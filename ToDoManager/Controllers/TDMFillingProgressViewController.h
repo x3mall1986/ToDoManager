51981370bd2d6633b6ee0fb26c00a0ce34ed72ea
Dima Shevchuk <dima.shevchuk@alterplay.com>
2013-05-31 14:38:14 +0300

    Refact with new fillController

---
+++ ToDoManager/Controllers/TDMFillingProgressViewController.h
@@ -0,0 +1,13 @@
+//
+//  TDMFillingProgressViewController.h
+//  ToDoManager
+//
+//  Created by Dmytro Shevchuk on 31.05.13.
+//  Copyright (c) 2013 PaksHome. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TDMFillingProgressViewController : UIViewController
+
+@end
---
+++ ToDoManager/Controllers/TDMFillingProgressViewController.m
@@ -0,0 +1,38 @@
+//
+//  TDMFillingProgressViewController.m
+//  ToDoManager
+//
+//  Created by Dmytro Shevchuk on 31.05.13.
+//  Copyright (c) 2013 PaksHome. All rights reserved.
+//
+
+#import "TDMFillingProgressViewController.h"
+
+@interface TDMFillingProgressViewController ()
+
+@end
+
+@implementation TDMFillingProgressViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
